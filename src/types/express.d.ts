baec76865d71b59ae1643c99f94b12938d51aeba
yswin-stack <yuvrajsinghwin@gmail.com>
2025-11-25 09:41:02 -0600

    Update express.d.ts

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,8 +3,10 @@ import "express";
 declare module "express-serve-static-core" {
   interface Request {
     user?: {
+      id: number;
       userId: number;
       role: "subscriber" | "driver" | "admin";
+      phone?: string | null;
     };
   }
 }
